4f2fa9fd56edd1154737e06ccafecf68510b857b
ryanlupague <ryan.lupague@indebted.co>
2024-07-08 14:08:07 +0800

    create main webapp script

--- main.py
+++ main.py
@@ -9,25 +9,53 @@ income, employee contributions, monthly tax, and monthly net income.
 
 from datetime import datetime
 
+import cv2
+import keras
+import numpy as np
+import pandas as pd
 import streamlit as st
+import tensorflow as tf
 
 from config.site_config import div_configs
-from utils.income_calculator import IncomeCalculator
 from utils.utils import SessionState
 
 ## ======================================================================================= ##
 ## SITE TITLE
+TITLE = "Ovarian Ultrasound Image Analysis Using Deep Learning"
+ICON = "🏥"
+QUICK_DESCRIPTION = ""
+# QUICK_DESCRIPTION = "Ovarian Ultrasound Image Analysis Using Deep Learning"
+
 st.set_page_config(
     initial_sidebar_state="collapsed",
-    page_title="PH Income Breakdown Calculator",
-    page_icon=":dollar:",
+    page_title=TITLE,
+    page_icon=ICON,
 )
+
+
+@st.cache_resource
+def load_model(path):
+    """
+    Load a Keras model from the given path.
+
+    Parameters:
+    path (str): The path to the saved model file.
+
+    Returns:
+    keras.models.Model: The loaded Keras model.
+    """
+    loaded_model = keras.models.load_model(path)
+    return loaded_model
+
+
+model = load_model("model/efficientnetV2B0_trained.keras")
+
 
 ## ======================================================================================= ##
 ## SITE CONFIGURATION
 
-st.markdown(div_configs["hide_github_icon"], unsafe_allow_html=True)
-st.markdown(div_configs["hide_streamlit_style"], unsafe_allow_html=True)
+# st.markdown(div_configs["hide_github_icon"], unsafe_allow_html=True)
+# st.markdown(div_configs["hide_streamlit_style"], unsafe_allow_html=True)
 
 
 ## ======================================================================================= ##
@@ -42,156 +70,126 @@ results = st.container()
 button = SessionState(key="button_clicked", default_state=False)
 
 with title:
-    st.title(":dollar: PH Income Breakdown Calculator")
-    st.subheader("A simple calculator for computing Net Income for Filipinos")
-    st.markdown(
-        f"""
-        > UPDATED AS OF {datetime.today().strftime('%B')} {datetime.today().year}
-        """
-    )
+    st.title(f" {ICON} {TITLE}")
+    if QUICK_DESCRIPTION != "":
+        st.subheader(QUICK_DESCRIPTION)
+
+    CLASS_NAMES = ["Benign", "Malignant"]
+
+
+# with contents:
+#     st.subheader("Upload Image/s:")
+#     st.markdown(
+#         """
+#         #### Upload up to 5 grayscale images here:
+#     """
+#     )
+#     grayscale_images = st.file_uploader(
+#         "Choose a .jpg or .png file", accept_multiple_files=True, key="grayscale_images"
+#     )
+
+#     st.markdown(
+#         """
+#         #### Upload up to 5 color doppler images here:
+#     """
+#     )
+#     doppler_images = st.file_uploader(
+#         "Choose a .jpg or .png file", accept_multiple_files=True, key="doppler_images"
+#     )
 
 
 with contents:
-    basic_salary = st.number_input(
-        label="Monthly Basic Income (PHP)",
-        min_value=1000,
-        max_value=None,
-        value="min",
-        step=1000,
+    st.subheader("Upload Image/s:")
+    ovarian_images = st.file_uploader(
+        "Choose a .jpg or .png file",
+        accept_multiple_files=True,
+        type=["png", "jpg"],
+        on_change=button.sessionstate_false,
     )
-    allowance = st.number_input(
-        label="Non-Taxable Allowance (De-minimis) (PHP)",
-        min_value=0,
-        max_value=None,
-        value="min",
-        step=1000,
-    )
-    nightdf_allow = st.checkbox("Night Differential Rate?")
-    if nightdf_allow:
-        night_differential_rate = st.number_input(
-            label="Night Differential Rate (%)",
-            min_value=0,
-            max_value=100,
-            value="min",
-            step=1,
-        )
-        night_differential_value = basic_salary * (night_differential_rate / 100)
-        income_calculator = IncomeCalculator(
-            basic_salary=basic_salary,
-            allowance=allowance,
-            night_differential=night_differential_value,
-        )
 
+    # IF no file is uploaded
+    if ovarian_images is None or ovarian_images == []:
+        st.write("Please upload the file")
     else:
-        income_calculator = IncomeCalculator(
-            basic_salary=basic_salary, allowance=allowance
-        )
+        st.write("File/s has uploaded succesfully")
 
-    income_details = income_calculator.compute_netincome()
-
-    submit_button = st.button(
-        label="Calculate", type="primary", on_click=button.sessionstate_true
+    evaluate_button = st.button(
+        label="Evaluate",
+        type="primary",
+        on_click=button.sessionstate_true,
+        use_container_width=True,
     )
 
 
 with results:
-    if submit_button or button.check_sessionstate():
-        st.subheader("NET INCOME")
+    if evaluate_button or button.check_sessionstate():
+        st.subheader("Result")
+
+        images_to_be_evaluated = {}
+        init_image_num = 1
+        for image in ovarian_images:
+            bytes_data = image.getvalue()
+            image = tf.image.decode_image(bytes_data)[..., :3]
+            resized_image = tf.image.resize(image, (224, 224))
+            image_array = tf.expand_dims(resized_image, 0)
+            prediction = model.predict(image_array, verbose=0)
+            predicted_value = CLASS_NAMES[np.argmax(prediction)]
+            images_to_be_evaluated[init_image_num] = {
+                "predicted_value": predicted_value,
+                "proba_0": prediction[0][0],
+                "proba_1": prediction[0][1],
+            }
+            init_image_num += 1
+
+        results_df = pd.DataFrame(images_to_be_evaluated).T
+
+        st.write()
+
+        count_1st_class = results_df["predicted_value"].value_counts()[0]
+        count_2nd_class = (
+            results_df["predicted_value"].value_counts()[1]
+            if len(results_df["predicted_value"].value_counts()) > 1
+            else 0
+        )
+
+        if count_1st_class == count_2nd_class:
+            results_df_grouped = results_df.groupby(
+                ["predicted_value"], as_index=False
+            ).agg(
+                predicted_counts=("predicted_value", "count"),
+                average_pred_0=("proba_0", "mean"),
+                average_pred_1=("proba_1", "mean"),
+            )
+            results_df_grouped = results_df_grouped.agg(
+                Benign=("average_pred_0", "max"),
+                Malignant=("average_pred_1", "max"),
+            )
+
+            benign_value = results_df_grouped["average_pred_0"].values[0]
+            malignant_value = results_df_grouped["average_pred_1"].values[1]
+
+            if benign_value > malignant_value:
+                overall_pred = "Benign"
+            else:
+                overall_pred = "Malignant"
+        else:
+            overall_pred = results_df["predicted_value"].value_counts().idxmax()
+
+        prediction_text_color = "green" if overall_pred == "Benign" else "red"
         st.markdown(
             f"""
-        ### Monthly Net Income: ₱ {income_details['Monthly Net Income']:,}
+        ##### {len(ovarian_images)} {'image was' if len(ovarian_images) == 1 else 'images were'}  uploaded, overall prediction is: :{prediction_text_color}[**{overall_pred.upper()}**]
         """
         )
 
-        monthly_tab, daily_tab, hourly_tab, yearly_tab = st.tabs(
-            ["Monthly", "Daily", "Hourly", "Yearly"]
+        st.write(results_df)
+
+        ## Generating heatmap
+
+        index_pred = list(
+            results_df[results_df["predicted_value"] == overall_pred].index
         )
+        index_pred = [value - 1 for value in index_pred]
 
-        with monthly_tab:
-            with st.expander("Income Breakdown", expanded=False):
-
-                st.markdown(
-                    f"""
-                #### Breakdown:
-                | Details | Amount |
-                |---------|--------|
-                | Basic Salary | ₱ {income_details['Basic Salary']:,} |
-                | Allowance | ₱ {income_details['Allowance']:,} |
-                | Night Differential | ₱ {float(income_details.get('Night Differential', 0)):,} |
-                | <span style="color:green"> **Gross Income** </span> | <span style="color:green"> **₱ {income_details['Monthly Gross Income']:,}** </span>|
-                | SSS Contribution | ₱ {income_details['SSS Contribution']:,} |
-                | PhilHealth Contribution | ₱ {income_details['PhilHealth Contribution']:,} |
-                | PAGIBIG Contribution | ₱ {income_details['PAGIBIG Contribution']:,} |
-                | <span style="color:red"> **Total Employee Contribution** </span> | <span style="color:red"> **₱ {income_details['Total Employee Contribution']:,}** </span> |
-                | <span style="color:red"> **Monthly Tax** </span> | <span style="color:red"> **₱ {income_details['Monthly Tax']:,}** </span> |
-                | <span style="color:green"> **Monthly Net Income** </span> | <span style="color:green"> **₱ {income_details['Monthly Net Income']:,}** </span> |
-                """,
-                    unsafe_allow_html=True,
-                )
-
-        with daily_tab:
-            st.markdown(
-                f"""
-            #### Daily Rate (Gross): ₱ {round((income_details['Monthly Gross Income']*12/261),2):,}
-            """
-            )
-            st.markdown(
-                f"""
-            #### Daily Rate (Net): ₱ {round((income_details['Monthly Net Income']*12/261),2):,}
-            """
-            )
-            st.divider()
-            st.markdown(
-                """
-            #### 🎉 Additional Holiday Rates:
-            """
-            )
-
-            if income_details["Monthly Tax"] == 0:
-                daily_basic = round((income_details["Basic Salary"] * 12 / 261), 2)
-            else:
-                daily_basic = (
-                    round((income_details["Basic Salary"] * 12 / 261), 2) * 0.8
-                )
-
-            st.markdown(
-                f"""
-            ##### Regular Holiday: + ₱{round(daily_basic,2):,} per day
-            """
-            )
-            st.markdown(
-                f"""
-            ##### Special Non-Working Holiday: + ₱ {round(daily_basic * .3,2):,} per day
-            """
-            )
-
-        with hourly_tab:
-            working_hours = st.number_input(
-                label="How many hours do you work in a day?",
-                min_value=1,
-                max_value=24,
-                value=8,
-                step=1,
-            )
-            st.markdown(
-                f"""
-            #### Hourly Rate (Gross): ₱ {round((income_details['Monthly Gross Income']*12/261/working_hours),2):,}
-            """
-            )
-            st.markdown(
-                f"""
-            #### Hourly Rate (Net): ₱ {round((income_details['Monthly Net Income']*12/261/working_hours),2):,}
-            """
-            )
-
-        with yearly_tab:
-            st.markdown(
-                f"""
-            #### Yearly Rate (Gross): ₱ {round((income_details['Monthly Gross Income']*12),2):,}
-            """
-            )
-            st.markdown(
-                f"""
-            #### Yearly Rate (Net): ₱ {round(((income_details['Monthly Gross Income']*12) - (income_details["Yearly Tax"]) - (income_details["Total Employee Contribution"]*12)),2):,}
-            """
-            )
+        for predicted_images_index in index_pred:
+            st.image(ovarian_images[predicted_images_index], width=500)
